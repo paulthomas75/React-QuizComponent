244090ba881674b1166ce4f7aa7f511d57ba8016
Paul T <paul@thomascomputing.co.uk>
2020-01-23 20:58:51 +0000

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react';
 let quizData = require('./quiz_data.json');
 class Quiz extends Component {
     constructor(props) {
-        super();
+        super(props);
         this.state = {quiz_position: 1};
     }
     render() {
         return "<div><div class='QuizQuestion'></div></div>"
     }
 }
-export default Quiz;
+export default Quiz
